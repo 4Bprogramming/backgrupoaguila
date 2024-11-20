39fd7b0d29282affb3be01fc187a50b65f962318
4Bprogramming <4b.programming@gmail.com>
2024-11-20 09:16:06 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -11,21 +11,12 @@ const {
 
 
 
- const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/aguilagrupo5`, {
-
-// const sequelize = new Sequelize(DATABASE_URL, {
+const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: false,// lets Sequelize know we can use pg-native for ~30% more speed
-   
-  // //extra configuration for heroku
-  //   dialectOptions:{
-  //   ssl:{
-  //     require:true,
-  //     rejectUnauthorized:false
-  //    }
-  //  },
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialect: "mysql",
 
-})
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
